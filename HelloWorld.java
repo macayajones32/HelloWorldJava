6187b90ba35129d0a1fed18a2e6064791e5fe83a
macayajones <macaya.jones@bison.howard.edu>
2020-02-04 02:00:13 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Today is February 4th, 2020. I am alive!");
+        System.out.println("My name is Macaya.");
+    }
+}
